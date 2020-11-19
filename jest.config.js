e453516ae6a1155be7ff50092fbb08903f36fac6
tarcisio.philips <tarcisio.philips@unifesp.br>
2020-11-19 12:35:00 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/src'],
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}

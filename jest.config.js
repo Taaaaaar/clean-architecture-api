a76c60b7878344332306a59c310d44ab8cb0ec40
tarcisio.philips <tarcisio.philips@unifesp.br>
2020-11-19 12:38:52 -0300

    chore: add jest env to eslint config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
-
 module.exports = {
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/src'],
